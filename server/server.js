11e30f0da8a94ba1ce2a2cfde6279b030f903891
Darwin <dhdo@ncsu.edu>
2017-10-03 03:50:04 -0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require('lodash');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var {mongoose} = require("./db/mongoose");
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {ObjectID} = require("mongodb");
-var app = express();
+const {mongoose} = require("./db/mongoose");
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {ObjectID} = require("mongodb");
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -85,9 +86,33 @@ app.delete('/todos/:id', (req, res) => {
       // 400 with empry body
       return res.status(400).send();
   });
+});
 
+app.patch('/todos/:id', (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text', 'completed']);
 
+   //validate the id -> not valid? return 404
+   if (!ObjectID.isValid(id)){
+     return res.status(404).send();
+   }
 
+   if (_.isBoolean(body.completed) && body.completed) {
+     body.completedAt = new Date().getTime();
+   } else {
+     body.completed= false;
+     body.completedAt=null;
+   }
+
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=> {
+     if( !todo) {
+       return res.status(404).send();
+     }
+
+     res.send({todo});
+   }).catch((e) => {
+     res.status(400).send();
+   })
 
 });
 
