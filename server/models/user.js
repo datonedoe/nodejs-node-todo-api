41934bffa310f43837c5eb7ae3f2ca1efc01f13d
Darwin <dhdo@ncsu.edu>
2018-02-01 01:58:58 -0800

    finished lec97 - hasing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
-
+const bcrypt = require("bcryptjs");
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -55,6 +55,44 @@ UserSchema.methods.generateAuthToken = function() {
       return token;
     })
 }
+
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+      decoded = jwt.verify(token, 'abc123');
+      console.log("decoded", decoded);
+  } catch (e) {
+    // retur new Promise((resolve, reject) => {
+    //   reject()
+    // })
+    return Promise.reject("Authentication failed");
+  }
+
+  return User.findOne({
+    "_id": decoded._id,
+    'tokens.token': token,
+    'tokens.access': "auth"
+  })
+}
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    //user.password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) =>{
+        user.password = hash;
+        next();
+      })
+    })
+
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require("./middleware/authenticate");
 const {ObjectID} = require("mongodb");
 const app = express();
 
@@ -122,6 +123,7 @@ app.patch('/todos/:id', (req, res) => {
 
 // POST /users
 app.post("/users", (req, res) => {
+  console.log('/users')
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -138,6 +140,12 @@ app.post("/users", (req, res) => {
     .catch((e) => {
       res.status(400).send(e);
     })
+})
+
+
+// GET /users/me
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 })
 
 app.listen( port, () => {
